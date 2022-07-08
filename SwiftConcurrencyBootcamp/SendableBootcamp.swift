e88a112043f8f1b25a4993f8a0c6712a87b869f9
Swiftful Thinking <ndsarno16@gmail.com>
2022-07-07 21:36:43 -0400

    [Feature] First commit

---
+++ SwiftConcurrencyBootcamp/SendableBootcamp.swift
@@ -0,0 +1,66 @@
+//
+//  SendableBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Nick Sarno on 4/4/22.
+//
+
+import SwiftUI
+
+actor CurrentUserManager {
+    
+    func updateDatabase(userInfo: MyClassUserInfo) {
+        
+    }
+    
+}
+
+struct MyUserInfo: Sendable {
+    var name: String
+}
+
+final class MyClassUserInfo: @unchecked Sendable {
+    private var name: String
+    let queue = DispatchQueue(label: "com.MyApp.MyClassUserInfo")
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func updateName(name: String) {
+        queue.async {
+            self.name = name
+        }
+    }
+}
+
+class SendableBootcampViewModel: ObservableObject {
+    
+    let manager = CurrentUserManager()
+    
+    func updateCurrentUserInfo() async {
+        
+        let info = MyClassUserInfo(name: "info")
+        
+        await manager.updateDatabase(userInfo: info)
+    }
+    
+}
+
+struct SendableBootcamp: View {
+    
+    @StateObject private var viewModel = SendableBootcampViewModel()
+    
+    var body: some View {
+        Text("Hello, World!")
+            .task {
+                
+            }
+    }
+}
+
+struct SendableBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        SendableBootcamp()
+    }
+}
--- SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftConcurrencyBootcampApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ActorsBootcamp()
         }
     }
 }
